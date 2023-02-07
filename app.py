cd9f1bc32936790e4803cecf4ee535be7045bad2
DMcDonald57 <d.mcdonald579@gmail.com>
2023-02-06 20:42:13 -0700

    all procedures work (except update) but once I add flask error.  I think my dbhelpers is missing something

--- app.py
+++ app.py
@@ -1,40 +1,33 @@
-import mariadb
-# from flask import Flask
-import dbcreds
-# import json
+from flask import Flask
+import json
+from dbhelpers import run_statement
 
-conn = mariadb.connect(
-            user=dbcreds.user,
-            password=dbcreds.password,
-            host=dbcreds.host,
-            port=dbcreds.port,
-            database=dbcreds.database,
-            autocommit = True
-            )
+app = Flask(__name__)
 
-cursor = conn.cursor()
-# app = Flask(__name__)
-
+@app.get('/animals')
 def get_animals():
-    cursor.execute("CALL get_animals")
-    result = cursor.fetchall()
-    print(result)
+    results = run_statement('CALL get_animals(?)',[])
+    if(type(results) == list):
+        get_animals_json = json.dumps(results, default=str)
+        return get_animals_json
+    else:
+        return 'There was an error'
 
-def add_animals():
-    name = input("Animal to add:\n")
-    cursor.execute("CALL add_animals(?)",[name])
-    cursor.execute("CALL get_animals")
-    result = cursor.fetchall()
-    print(result)
+# def add_animals():
+#     name = input("Animal to add:\n")
+#     cursor.execute("CALL add_animals(?)",[name])
+#     cursor.execute("CALL get_animals")
+#     result = cursor.fetchall()
+#     print(result)
 
-def remove_animals():
-    name = input("Animal to remove:\n")
-    cursor.execute("CALL remove_animals(?)",[name])
-    cursor.execute("CALL get_animals")
-    result = cursor.fetchall()
-    print(result)
+# def remove_animals():
+#     name = input("Animal to remove:\n")
+#     cursor.execute("CALL remove_animals(?)",[name])
+#     cursor.execute("CALL get_animals")
+#     result = cursor.fetchall()
+#     print(result)
 
 get_animals()
-add_animals()
-remove_animals()
-# app.run(debug=True)
+# add_animals()
+# remove_animals()
+app.run(debug=True)
